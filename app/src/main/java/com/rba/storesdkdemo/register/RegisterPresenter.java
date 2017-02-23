b76e96763331bb2961d39baf4e4b622e143a3102
Ricardo Bravo <ricardobravo@outlook.com>
2017-02-23 15:14:37 -0500

    chore(init): initial commit

---
+++ app/src/main/java/com/rba/storesdkdemo/register/RegisterPresenter.java
@@ -0,0 +1,57 @@
+package com.rba.storesdkdemo.register;
+
+import android.content.Context;
+
+import com.rba.storesdkdemo.util.Util;
+
+import pe.com.orbis.storesdk.model.request.RegisterRequest;
+import pe.com.orbis.storesdk.model.response.ErrorResponse;
+import pe.com.orbis.storesdk.model.response.RegisterResponse;
+import pe.com.orbis.storesdk.service.register.RegisterCallback;
+import pe.com.orbis.storesdk.service.register.RegisterService;
+
+/**
+ * Created by Ricardo Bravo on 31/01/17.
+ */
+
+public class RegisterPresenter implements RegisterCallback {
+
+    private RegisterView registerView;
+
+    public RegisterPresenter(RegisterView registerView){
+        this.registerView = registerView;
+    }
+
+    public void register(RegisterRequest registerRequest) {
+        registerView.showLoading();
+        RegisterService.register(registerRequest, this);
+    }
+
+    public boolean validEmail(String email){
+        return Util.validEmail(email);
+    }
+
+    public boolean validPassword(String password){
+        return Util.validPassword(password);
+    }
+
+    public boolean validName(String name){
+        return Util.validName(name);
+    }
+
+    public boolean validSurname(String surname){
+        return Util.validName(surname);
+    }
+
+    @Override
+    public void onRegisterResponse(RegisterResponse registerResponse) {
+        registerView.hideLoading();
+
+    }
+
+    @Override
+    public void onRegisterError(ErrorResponse errorResponse) {
+        registerView.hideLoading();
+        registerView.showErrorMessage(errorResponse.get_meta().getMessage());
+    }
+}
---
+++ app/src/main/java/com/rba/storesdkdemo/util/Util.java
@@ -0,0 +1,30 @@
+package com.rba.storesdkdemo.util;
+
+import android.util.Patterns;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by Ricardo Bravo on 31/01/17.
+ */
+
+public class Util {
+
+    public static boolean validEmailPattern(String email){
+        Pattern pattern = Patterns.EMAIL_ADDRESS;
+        return pattern.matcher(email).matches();
+    }
+
+    public static boolean validEmail(String email){
+        return validEmailPattern(email.trim());
+    }
+
+    public static boolean validPassword(String password) {
+        return password.length()>5;
+    }
+
+    public static boolean validName(String name) {
+        return name.length()>2;
+    }
+
+}
